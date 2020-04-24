7dd6ff1c280227d6811079ab1414320cccf4d6ec
winston.shih88 <winston.shih88@gmail.com>
2020-04-23 23:39:22 -0700

    completed challenge, cleaned code.

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,56 +1,56 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments(
-	dept_no VARCHAR (4) NOT NULL,
-	dept_name VARCHAR (40) NOT NULL,
+		dept_no VARCHAR (4) NOT NULL,
+		dept_name VARCHAR (40) NOT NULL,
 	PRIMARY KEY (dept_no),
 	UNIQUE (dept_name)
 );
 
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL, 
+		emp_no INT NOT NULL,
+		birth_date DATE NOT NULL,
+		first_name VARCHAR NOT NULL,
+		last_name VARCHAR NOT NULL,
+		gender VARCHAR NOT NULL,
+		hire_date DATE NOT NULL, 
 	PRIMARY KEY(emp_no),
 	UNIQUE (emp_no)
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+		dept_no VARCHAR(4) NOT NULL,
+		emp_no INT NOT NULL,
+		from_date DATE NOT NULL,
+		to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+		emp_no INT NOT NULL,
+		dept_no VARCHAR NOT NULL,
+		from_date DATE NOT NULL,
+		to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY(emp_no, dept_no)
 );
 
 CREATE TABLE titles(
-emp_no INT NOT NULL,
-title VARCHAR NOT NULL,
-from_date DATE NOT NULL,
-to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+		emp_no INT NOT NULL,
+		title VARCHAR NOT NULL,
+		from_date DATE NOT NULL,
+		to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no, title, from_date)
 );
 
 CREATE TABLE salaries (
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+		emp_no INT NOT NULL,
+		salary INT NOT NULL,
+		from_date DATE NOT NULL,
+		to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
@@ -58,11 +58,13 @@ CREATE TABLE salaries (
 SELECT*FROM dept_manager
 
 -- Retirement elegibility
-SELECT emp_no, first_name, last_name
+SELECT emp_no, 
+	   first_name, 
+	   last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Check the table
 SELECT * FROM retirement_info;
@@ -71,30 +73,31 @@ DROP TABLE retirement_info;
 
 -- Joining departments and dep_manager tables
 SELECT d.dept_name,
-	dm.emp_no,
-	dm.from_date,
-	dm.to_date
+	   dm.emp_no,
+	   dm.from_date,
+	   dm.to_date
 FROM department as d
 INNER JOIN dept_manager as dm
-ON d.dept_no = dm.dept_no;
+	ON d.dept_no = dm.dept_no;
 
 -- Joining retirement_info and dept_emp tables w/ alias
 SELECT ri.emp_no,
-	ri.first_name,
-	ri.last_name,
-	de.to_date
+	   ri.first_name,
+	   ri.last_name,
+	   de.to_date
 INTO current_emp
 FROM retirement_info as ri
 LEFT JOIN dept_emp as de
-ON ri.emp_no = de.emp_no
+	ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
 
 -- Employee count by department number
-SELECT COUNT(ce.emp_no), de.dept_no
+SELECT COUNT(ce.emp_no), 
+			de.dept_no
 INTO skill_drill
 FROM current_emp as ce
 LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
+	ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
@@ -104,37 +107,40 @@ SELECT * FROM salaries
 ORDER BY to_date DESC;
 
 --Constructing the emp_info list
-SELECT emp_no, first_name, last_name, gender
+SELECT emp_no, 
+	   first_name, 
+	   last_name, 
+	   gender
 INTO emp_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Joining emp info and salaries
 SELECT e.emp_no,
-e.first_name,
-e.last_name,
-e.gender,
-s.salary,
-de.to_date
+	   e.first_name,
+	   e.last_name,
+	   e.gender,
+	   s.salary,
+	   de.to_date
 INTO emp_info
 FROM employees as e
 INNER JOIN salaries as s
-ON (e.emp_no = s.emp_no)
+	ON (e.emp_no = s.emp_no)
 INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
+	ON (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
-AND (de.to_date = '9999-01-01');
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
 
 -- List of managers per department
 SELECT dm.dept_no,
-		d.dept_name,
-		dm.emp_no,
-		ce.last_name,
-		ce.first_name,
-		dm.from_date,
-		dm.to_date
+	   d.dept_name,
+	   dm.emp_no,
+	   ce.last_name,
+	   ce.first_name,
+	   dm.from_date,
+	   dm.to_date
 --INTO manager_info
 FROM dept_manager AS dm
 	INNER JOIN departments AS d
@@ -144,38 +150,38 @@ FROM dept_manager AS dm
 		
 -- List 3 of Department Retirees
 SELECT ce.emp_no,
-ce.first_name,
-ce.last_name,
-d.dept_name
+	   ce.first_name,
+	   ce.last_name,
+	   d.dept_name
 --INTO dept_info
 FROM current_emp AS ce
 INNER JOIN dept_emp AS de
 	ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
-ON (de.dept_no = d.dept_no);
+	ON (de.dept_no = d.dept_no);
 
 
 --Skill drill
 SELECT ce.emp_no,
-ce.first_name,
-ce.last_name,
-d.dept_name
+	   ce.first_name,
+	   ce.last_name,
+	   d.dept_name
 FROM current_emp AS ce
 INNER JOIN dept_emp AS de
 	ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
-ON (de.dept_no = d.dept_no)
+	ON (de.dept_no = d.dept_no)
 WHERE d.dept_name = 'Sales';
 
 SELECT ce.emp_no,
-ce.first_name,
-ce.last_name,
-d.dept_name
+	   ce.first_name,
+	   ce.last_name,
+	   d.dept_name
 FROM current_emp AS ce
 INNER JOIN dept_emp AS de
 	ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
-ON (de.dept_no = d.dept_no)
+	ON (de.dept_no = d.dept_no)
 WHERE d.dept_name IN ('Sales','Development')
 
 --Challenge
@@ -184,11 +190,11 @@ SELECT * FROM salaries
 SELECT * FROM employees
 --Technical Analysis Deliverable 1: No of Retiring Employees by Title.
 SELECT e.emp_no,
-e.first_name,
-e.last_name,
-ti.title,
-ti.from_date,
-s.salary
+	   e.first_name,
+	   e.last_name,
+	   ti.title,
+	   ti.from_date,
+	   s.salary
 INTO emp_title
 FROM employees AS e
 INNER JOIN titles AS ti
@@ -196,7 +202,7 @@ INNER JOIN titles AS ti
 INNER JOIN salaries AS s
 	ON (e.emp_no = s.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+		AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 ORDER BY from_date DESC;
 
 SELECT * FROM emp_title
@@ -206,7 +212,7 @@ SELECT *
 INTO emp_ret_list
 FROM (
    SELECT *,
-          row_number() over (PARTITION BY from_date ORDER BY emp_no) AS row_number
+          row_number() over (PARTITION BY emp_no ORDER BY emp_no) AS row_number
    FROM emp_title
    ) AS rows
 WHERE row_number = 1;
@@ -228,7 +234,7 @@ SELECT*FROM emp_ret_count
 
 --Position retiring count
 SELECT COUNT (emp_no),
-title
+			  title
 INTO title_ret_count
 FROM emp_ret_list
 GROUP BY title;
@@ -239,15 +245,15 @@ SELECT*FROM title_ret_count
 SELECT*FROM titles
 
 SELECT e.emp_no, 
-	e.first_name, 
-	e.last_name,
-	ti.title,
-	ti.from_date, 
-	ti.to_date
+	   e.first_name, 
+	   e.last_name,
+	   ti.title,
+	   ti.from_date, 
+	   ti.to_date
 INTO mentorship
 FROM employees as e
 INNER JOIN titles AS ti
-ON (e.emp_no = ti.emp_no)
+	ON (e.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31');
 
 --Removing duplicates
@@ -255,15 +261,16 @@ SELECT *
 INTO mentor_final
 FROM (
    SELECT *,
-          row_number() over (PARTITION BY from_date ORDER BY emp_no) AS row_number
+          row_number() over (PARTITION BY emp_no ORDER BY emp_no) AS row_number
    FROM mentorship
    ) AS rows
 WHERE row_number = 1;
 
 SELECT*FROM mentor_final
 
--- Remove column
+-- Remove column ref: https://www.w3schools.com/sql/sql_ref_drop_column.asp
 ALTER TABLE mentor_final
 DROP COLUMN row_number;
 
 SELECT*FROM mentor_final
+INTO mentor_final
